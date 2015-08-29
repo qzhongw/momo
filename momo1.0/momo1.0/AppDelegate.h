9ad07d0640d7e9ab59e985fc283b6bb8c3a18ad5
“qzwgit config —global user.email “qiuzhongwei@163.com <qzw@qzwdeMacBook-Air.local>
2015-08-29 19:36:36 +0800

    初始化仓库

---
+++ momo1.0/momo1.0/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo1.0
+//
+//  Created by qzw on 15/8/29.
+//  Copyright (c) 2015年 qzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo1.0/momo1.0/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  momo1.0
+//
+//  Created by qzw on 15/8/29.
+//  Copyright (c) 2015年 qzw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
